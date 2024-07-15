8187497991c3d4479d029d90d71f90768e548c6e
Laura Vidal <lauravidal@outlook.com>
2024-07-14 22:59:59 -0300

    jogo finalizado

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import random
-import csv
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
@@ -102,27 +101,6 @@ def restartGame():
     direcao = 'down'
     label.config(text="Score:{}".format(score))
     nextTurn(snake, maca)
-
-def inserindoScore(nome, score):
-    dados = []
-    with open("score.csv", "r") as arquivo:
-        leitor = csv.reader(arquivo)
-        for linha in leitor:
-            dados.append([linha[0], int(linha[1])])
-    
-    inserido = False
-    for i in range(len(dados)):
-        if score >= dados[i][1]:
-            dados.insert(i, [nome, score])
-            inserido = True
-            break
-    
-    if not inserido:
-        dados.append([nome, score])
-
-    with open("score.csv", "w", newline="") as arquivo:
-        escritor = csv.writer(arquivo)
-        escritor.writerows(dados)
 
 janela = Tk()
 janela.title("Snake Game")
